d101c7c6074d9f0d29fe509469efe25ef2632bd2
Administrator <ig1982.175@yandex.ru>
2022-09-05 22:51:40 +0500

    add task10

---
+++ Task10/Program.cs
@@ -0,0 +1,20 @@
+﻿//  Напишите программу, которая принимает на вход трёхзначное число 
+//  и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+
+Console.Clear();
+Console.Write("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int MaxDigit(int number)
+{
+    int firstDigit = (number % 100) / 10;
+    return firstDigit;
+}
+int result = MaxDigit(number);
+Console.WriteLine($"Вторая цифра числа - {result}");
+
+
+
