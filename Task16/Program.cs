448ed1509b2b661bb67f4bc759b4ca7c7970f137
Administrator <ig1982.175@yandex.ru>
2022-09-06 23:51:20 +0500

    add task16

---
+++ Task16/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу которая принимает на вход число
+// и проверяет кратно ли оно одновременно 7 и 23.
+// 14 -> нет
+// 46 -> нет
+// 161 -> да
+
+Console.Clear();
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int a = 7, b = 23;
+
+if (num % a == 0 && num % b == 0)
+{
+    Console.WriteLine($"Число {num} кратно {a} и {b}");
+}
+else 
+{
+    Console.WriteLine($"Число {num} не кратно {a} и {b}");
+}
