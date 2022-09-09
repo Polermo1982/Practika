a96ab3b6b4046baa49daaabaea6d2ca6b5c0a833
Administrator <ig1982.175@yandex.ru>
2022-09-09 17:34:21 +0500

    add task18

---
+++ Task18/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+
+Console.WriteLine("Введите номер четверти");                              
+int x = Convert.ToInt32(Console.ReadLine());
+
+string Quarter(int xc)                           
+{
+    if (xc == 1) return "x>0, y>0";                           
+    if (xc == 2) return "x<0, y>0";                        
+    if (xc == 3) return "x<0, y<0";       
+    if (xc == 4) return "x>0, y<0";    
+    return "Введите число от 1 до 4";
+}
+
+string result = Quarter(x);                    
+Console.WriteLine(result);
