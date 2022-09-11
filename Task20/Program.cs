5048199c59a685c31fd86dba9161cd211a956cda
Administrator <ig1982.175@yandex.ru>
2022-09-11 09:42:14 +0500

    add task23

--- Task20/Program.cs
+++ Task20/Program.cs
@@ -10,7 +10,7 @@ Console.Write("x1: ");
 int x1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("y1: ");
 int y1 = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите координаты второй точкиЖ ");
+Console.Write("Введите координаты второй точки: ");
 Console.Write("x2: ");
 int x2 = Convert.ToInt32(Console.ReadLine());
 Console.Write("y2: ");
