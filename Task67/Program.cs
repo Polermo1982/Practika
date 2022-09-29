f7af9110718c1d1a57cfd8c8a63fb3e8cbbd1b0a
Administrator <ig1982.175@yandex.ru>
2022-09-29 22:10:25 +0500

    add task67

---
+++ Task67/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 67: Напишите программу, которая будет принимать на вход число и
+// возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int SumDigits(int number)
+{
+    int sum = number % 10;
+    if (number != 0)
+    {
+        sum += SumDigits(number / 10);
+    }
+    return sum;
+}
+int result = SumDigits(num);
+Console.WriteLine(result);
